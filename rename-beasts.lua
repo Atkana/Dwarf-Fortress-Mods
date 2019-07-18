d62b4ef1cf8af3ac08649825c1aa0766b9f12597
Atkana <Em0srawk@live.com>
2019-07-18 12:02:53 +0100

    Add other generated creatures

--- rename-beasts.lua
+++ rename-beasts.lua
@@ -1,13 +1,16 @@
--- Changes the name of a world's Forgotten Beasts.
+-- Changes the name of a world's generated creatures.
 
 --[====[
 
 rename-beasts
 ===========
-Permanently changes the names of all Forgotten Beasts in a given save.
-Requires ``-singular`` ``-plural`` and ``-adjective`` entry for the new names.
+Permanently changes the names of all of a given type of generated creature in the current save.
+Requires `-singular` `-plural` and `-adjective` entry for the new names, and
+a `-type` (what creature's name to change).
+Valid types: FORGOTTEN_BEAST, DEMON, UNIQUE_DEMON, ANGEL, NIGHT_TROLL, TITAN,
+BOGEYMAN, WEREBEAST
 
-Example: ``rename-beasts -singular "Fun Beast" -plural "Fun Beasts" -adjective "Fun Beast"``
+Example: ``rename-beasts -type FORGOTTEN_BEAST -singular "Fun beast" -plural "Fun beasts" -adjective "Fun beast"``
 
 ]====]
 local utils=require('utils')
@@ -16,12 +19,29 @@ local validArgs = utils.invert({
  'singular',
  'plural',
  'adjective',
+ 'type',
 })
+local validTypes = {
+	FORGOTTEN_BEAST = true,
+	DEMON = true,
+	UNIQUE_DEMON = true,
+	ANGEL = true,
+	NIGHT_TROLL = true,
+	TITAN = true,
+	BOGEYMAN = true,
+	WEREBEAST = true,
+}
 
 local args = utils.processArgs({...}, validArgs)
 
 if not args.singular or not args.plural or not args.adjective then
 	qerror("You must enter names for singular, plural, and adjectives.")
+end
+
+args.type = string.upper(args.type or "")
+
+if validTypes[args.type] == nil  then
+	qerror("You must provide a valid type to target. Valid types: FORGOTTEN_BEAST, DEMON, UNIQUE_DEMON, ANGEL, NIGHT_TROLL, TITAN, BOGEYMAN, WEREBEAST.")
 end
 
 if not dfhack.isWorldLoaded() then
@@ -63,7 +83,27 @@ end
 
 local count = 0
 for index, creatureRaw in pairs(df.global.world.raws.creatures.all) do
-	if creatureRaw.flags.CASTE_FEATURE_BEAST == true then
+	local hit = false
+	
+	if args.type == "FORGOTTEN_BEAST" and creatureRaw.flags.CASTE_FEATURE_BEAST == true then
+		hit = true
+	elseif args.type == "DEMON" and creatureRaw.flags.CASTE_DEMON == true and creatureRaw.flags.CASTE_UNIQUE_DEMON == false then
+		hit = true
+	elseif args.type == "UNIQUE_DEMON" and creatureRaw.flags.CASTE_UNIQUE_DEMON == true then
+		hit = true
+	elseif args.type == "ANGEL" and creatureRaw.source_hfid ~= -1 then
+		hit = true
+	elseif args.type == "NIGHT_TROLL" and creatureRaw.flags.CASTE_NIGHT_CREATURE_ANY == true and creatureRaw.flags.CASTE_NIGHT_CREATURE_HUNTER == true and (creatureRaw.caste[0].flags.CONVERTED_SPOUSE == true or creatureRaw.caste[0].flags.SPOUSE_CONVERTER == true) then
+		hit = true
+	elseif args.type == "TITAN" and creatureRaw.flags.CASTE_TITAN == true then
+		hit = true
+	elseif args.type == "BOGEYMAN" and creatureRaw.flags.CASTE_NIGHT_CREATURE_BOGEYMAN == true then
+		hit = true
+	elseif args.type == "WEREBEAST" and creatureRaw.flags.CASTE_NIGHT_CREATURE_ANY == true and creatureRaw.flags.CASTE_NIGHT_CREATURE_HUNTER == true and creatureRaw.caste[0].flags.CONVERTED_SPOUSE == false and creatureRaw.caste[0].flags.SPOUSE_CONVERTER == false then 
+		hit = true
+	end
+	
+	if hit == true then
 		renameCreature(creatureRaw, args.singular, args.plural, args.adjective, true, true)
 		count = count + 1
 	end
